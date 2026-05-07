bda9e52fa8e4ae361aea435d5a2b02fa1e1c4a1e
agyintom <tom.agyin@outlook.com>
2026-05-07 15:35:46 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -16,7 +16,7 @@ resource "random_string" "suffix" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "6.6.1"
+  version = "~> 5.0"
 
   name                 = "aj-eks-vpc"
   cidr                 = var.vpc_cidr
