18538cd0904ca93545d83d57dcac6bdf3f0ede0b
Thomas Agyin Jnr <agyintom@outlook.com>
2026-05-14 14:54:18 +0200

    updated eks-cluster.tf

--- variables.tf
+++ variables.tf
@@ -11,3 +11,18 @@ variable "aws_region" {
   default = "eu-central-1"
   description = "aws region"
 }
+
+variable "github_actions_iam_user_arn" {
+  description = "ARN of the GitHub Actions IAM user"
+  type        = string
+}
+
+variable "admin_iam_user_arn" {
+  description = "ARN of the admin IAM user"
+  type        = string
+}
+
+variable "node_group_role_arn" {
+  description = "ARN of the EKS node group IAM role"
+  type        = string
+}
