38f8dafe444728c62311a1a0c820b49ff14ae5dd
Thomas Agyin Jnr <agyintom@outlook.com>
2026-05-14 14:51:27 +0200

    updated eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,11 +1,10 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 20.34"
-  cluster_name    = local.cluster_name
-  cluster_version = var.kubernetes_version
-  subnet_ids      = module.vpc.private_subnets
-
-  enable_irsa = true
+  source           = "terraform-aws-modules/eks/aws"
+  version          = "~> 20.34"
+  cluster_name     = local.cluster_name
+  cluster_version  = var.kubernetes_version
+  subnet_ids       = module.vpc.private_subnets
+  enable_irsa      = true
 
   tags = {
     cluster = "demo"
@@ -20,12 +19,50 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-
     node_group = {
       min_size     = 2
       max_size     = 6
       desired_size = 2
     }
   }
-}
 
+  # ---- Access Entries ----
+  authentication_mode = "API_AND_CONFIG_MAP"
+
+  access_entries = {
+    github_actions = {
+      principal_arn = var.github_actions_iam_user_arn
+      type          = "STANDARD"
+      username      = "github-actions"
+
+      policy_associations = {
+        edit = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSEditPolicy"
+          access_scope = {
+            type       = "namespace"
+            namespaces = ["default"]
+          }
+        }
+      }
+    }
+
+    admin = {
+      principal_arn = var.admin_iam_user_arn
+      type          = "STANDARD"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+
+    node_group = {
+      principal_arn = var.node_group_role_arn
+      type          = "EC2_LINUX"
+    }
+  }
+}
